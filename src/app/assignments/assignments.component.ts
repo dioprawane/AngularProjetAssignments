0ceae371b839cd78388ab30ce35fdab54829f8df
Diop Serigne Rawane <serigne-rawane.diop@etu.unice.fr>
2024-01-23 21:01:35 +0100

    23/01/2024 : fin de journée

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -6,7 +6,6 @@ import { Router } from '@angular/router';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort, Sort } from '@angular/material/sort';
-import { MatRadioChange } from '@angular/material/radio';
 
 @Component({
   selector: 'app-assignments',
@@ -141,17 +140,15 @@ export class AssignmentsComponent implements OnInit, AfterViewInit {
   applyFilter(filterValue: string) {
     switch (filterValue) {
       case 'all':
-        this.getAssignments();
+        this.flatAssignments.data = [...this.originalAssignments]; // Réinitialiser avec les données originales
         break;
       case 'rendu':
-        this.flatAssignments.data = this.flatAssignments.data.filter(a => a.rendu);
-        //this.assignments = this.originalAssignments.filter(a => a.rendu);
-        break;
       case 'nonRendu':
-        this.flatAssignments.data = this.flatAssignments.data.filter(a => !a.rendu);
-        //this.assignments = this.originalAssignments.filter(a => !a.rendu);
+        const isRendu = filterValue === 'rendu';
+        this.flatAssignments.data = this.originalAssignments.filter(a => a.rendu === isRendu);
         break;
     }
+    this.changeDetectorRefs.detectChanges(); // Informer Angular du changement
   }
 
 }
